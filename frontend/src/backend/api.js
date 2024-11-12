ce1608889246efc33ca3122d5f72d19b24edfa1d
dharanigk2002 <dharanigk2018@gmail.com>
2024-11-12 14:27:15 +0530

    modify

--- frontend/src/backend/api.js
+++ frontend/src/backend/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = "https://admin-dashboard-xso8.onrender.com";
 
 export async function getCurrentUser() {
   const response = await fetch(`${BASE_URL}/api/admin/valid`, {
